4a87de0c12f0b8572299b943a4ab17ec2077f306
Lraq-1991 <alvarezquinteroluis@gmail.com>
2026-05-01 16:02:01 -0300

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    const greeting = "Hi Mai!<br><br>What are you doing?"
     return 'Hello World!';
   }
 }
