1023e1f5336e8b7df1840049952fc19be8a8a8b7
Lraq-1991 <alvarezquinteroluis@gmail.com>
2026-05-02 15:11:04 -0300

    Pages skeletons

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  getMainPage() {
+    return { title: 'Home | Programming Portfolio' };
+  }
+
+  @Get('resume')
+  @Render('resume')
+  getResumePage() {
+    return { title: 'Resume | Programming Portfolio' };
+  }
+
+  @Get('projects')
+  @Render('projects')
+  getProjectsPage() {
+    return { title: 'Projects | Programming Portfolio' };
+  }
+
+  @Get('contact')
+  @Render('contact')
+  getContactPage() {
+    return { title: 'Contact | Programming Portfolio' };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Serve static assets (CSS, Images)
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  
+  // Set up the views directory and template engine
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  await app.listen(3000);
+  console.log(`Application is running on: http://localhost:3000`);
 }
 bootstrap();
