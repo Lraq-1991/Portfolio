603055dfef60de50086c185d547624016b9da41a
Lraq-1991 <alvarezquinteroluis@gmail.com>
2026-05-04 14:31:42 -0300

    CSS Edition

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./src/**/*.ts"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'concrete': '#f3f3f3',
+        'cold-gray': '#0d0d0d',
+        'yellow-sea': '#eea404',
+        'red-orange': '#fe3b25',
+        'cold-gray': '#84dadb',
+      }
+    },
   },
-  plugins: [],
+  plugins: [require('tw-elements/dist/plugin.cjs')],
+  darkMode: 'class'
 }
+
