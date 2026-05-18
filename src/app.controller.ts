7c0c920d9951f31cdca97b6d1dc5cc3b8a41ba10
Lraq-1991 <alvarezquinteroluis@gmail.com>
2026-05-18 18:52:35 -0300

    Projects routing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,24 +5,48 @@ export class AppController {
   @Get()
   @Render('index')
   home() {
-    return { title: 'Home | cv', active: 'home' };
+    return { title: 'Home', active: 'home' };
   }
 
   @Get('resume')
   @Render('resume')
   resume() {
-    return { title: 'Resumo | cv', active: 'resumo' };
+    return { title: 'Resumo', active: 'resumo' };
   }
 
   @Get('projects')
   @Render('projects')
   projects() {
-    return { title: 'Projetos | cv', active: 'projetos' };
+    return { title: 'Projetos', active: 'projetos' };
   }
 
   @Get('contact')
   @Render('contact')
   contact() {
-    return { title: 'Contato | cv', active: 'contato' };
+    return { title: 'Contato', active: 'contato' };
+  }
+
+   @Get('projects/eda-analysis')
+  @Render('./projects/eda')
+  eda() {
+    return { title: 'EDA Analysis', active: 'projetos' };
+  }
+
+   @Get('projects/advanced-queries')
+  @Render('./projects/tsql')
+  tsql() {
+    return { title: 'Advanced Querys', active: 'projetos' };
+  }
+
+   @Get('projects/query-tuning')
+  @Render('./projects/performance')
+  performance() {
+    return { title: 'Query Tuning', active: 'projetos' };
+  }
+
+   @Get('projects/pbi-visuals')
+  @Render('./projects/powerbi')
+  visuals() {
+    return { title: 'Visual with Power BI', active: 'projetos' };
   }
 }
