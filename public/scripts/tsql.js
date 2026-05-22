e33ef0754a215988dbc485def7ff87783cc52c8d
Lraq-1991 <alvarezquinteroluis@gmail.com>
2026-05-22 18:54:42 -0300

    update

---
+++ public/scripts/tsql.js
@@ -0,0 +1,17 @@
+import * as data from '../assets/queries.json';
+
+const fs = require('fs');
+
+const query1 = data.queries[0];
+
+
+
+//test
+console.log(query1);
+
+/*
+//Create template to render data
+getElementById('query-tittle').innerHTML = `
+    <h3 class="mt-2 font-worksansSemi text-footerlabel text-ink">${query1.tittle}</h3>
+`;  
+*/
