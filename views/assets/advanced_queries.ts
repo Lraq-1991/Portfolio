f859aaa4ce664e90fc3d024eb0063f5751252e69
Lraq-1991 <alvarezquinteroluis@gmail.com>
2026-05-22 17:40:35 -0300

    structuring
    Organizing files to use them in views

---
+++ views/assets/advanced_queries.ts
@@ -0,0 +1,13 @@
+interface QueryObject {
+    tittle: string;
+    query_text: string;
+    query_output: string;
+}
+
+const query1: QueryObject = {
+    tittle: "Query 1",
+    query_text: "Query text 1",
+    query_output: "Query output 1"
+}
+
+export default query1
