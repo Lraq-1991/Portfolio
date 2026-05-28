d15609b290d637165f4a5bcb52193c05e67e38b7
Lraq-1991 <alvarezquinteroluis@gmail.com>
2026-05-28 19:33:05 -0300

    Actions and Style - First Project Page

--- public/scripts/tsql.js
+++ public/scripts/tsql.js
@@ -1,21 +1,45 @@
-import query1 from '../assets/query1.js';
+import queries from '../assets/queries.js';
 
 
 //test
-console.log(query1);
+console.log(queries);
+
+// First load
+window.onload = function() {
+
+  document.querySelector('.list-button').click();
+
+}
+
+// Get list buttons
+const buttons = document.querySelectorAll('.list-button');
+
+// Alter content based on list button
+buttons.forEach(button => {
+  button.addEventListener('click', function() {
+
+    
+    buttons.forEach(button => {
+        button.style.backgroundColor = '';
+      });
+
+    this.style.backgroundColor = 'gray';
+
+    const text = this.getAttribute('data-text');
+    
+    document.getElementById('query-tittle').innerHTML = `
+    <h3 class="mt-2 font-worksansSemi text-footerlabel text-ink">${queries[text].tittle}</h3>
+    `;
+
+    document.getElementById('query-text').innerHTML = `
+    <p class="mt-2 font-avenir text-body leading-[1.6em] text-ink">${queries[text].query_text}</p>
+    `;
+
+    document.getElementById('query-output').innerHTML = `
+    <p class="mt-2 font-avenir text-body leading-[1.6em] text-ink">${queries[text].query_output}</p>
+    `;
+
+  });
+});
 
 
-//Create template to render data
-let queryTittle = document.getElementById('query-tittle').innerHTML = `
-    <h3 class="mt-2 font-worksansSemi text-footerlabel text-ink">${query1.tittle}</h3>
-`;  
-
-
-let queryText = document.getElementById('query-text').innerHTML = `
-    <p class="mt-2 font-avenir text-body leading-[1.6em] text-ink">${query1.query_text}</p>
-`;  
-
-
-let queryOutput = document.getElementById('query-output').innerHTML = `
-    <p class="mt-2 font-avenir text-body leading-[1.6em] text-ink">${query1.query_output}</p>
-`;
